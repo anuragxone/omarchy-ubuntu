8ae95235b6661369683ed3c5249950ad04c1979b
Anurag Kumar <anuragxone@outlook.in>
2025-11-09 11:18:25 +0000

    fix: add apt-transport-https and sudo

--- install-taskfile.sh
+++ install-taskfile.sh
@@ -1,3 +1,3 @@
-sudo apt-get install -y curl bash
+sudo apt-get install -y curl bash apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/task/task/setup.deb.sh' | sudo -E bash
-apt install task
+sudo apt install task
